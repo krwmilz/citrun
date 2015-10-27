d8d96e4c782f8f1a1d9151dfab4a49681955040d
kyle <kyle@getaddrinfo.net>
2015-10-26 20:20:48 -0600

    tests: initial commit

---
+++ run_tests.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+make || exit 1
+# make sure we have a .c extension
+temp_file=$(mktemp).c
+
+if which tput > /dev/null; then
+	RED=`tput setaf 1 0 0`
+	GREEN=`tput setaf 2 0 0`
+	RESET=`tput sgr0`
+fi
+
+echo "starting tests"
+for t in `ls tests/*.c`; do
+	./instrument $t -- > $temp_file
+	if ! diff -u ${t}.instrumented $temp_file; then
+		echo "$t:$RED source compare failed$RESET"
+		continue
+	fi
+
+	if ! gcc -o /tmp/bin $temp_file; then
+		# /tmp/bin won't be created here
+		echo "$t:$RED post compilation failed$RESET"
+		continue
+	fi
+
+	if ! sh ${t}.sh /tmp/bin; then
+		echo "$t:$RED tests failed!"
+		rm /tmp/bin
+		continue
+	fi
+	rm /tmp/bin
+
+	echo "$t:$GREEN ok$RESET"
+done
+
+rm $temp_file
---
+++ tests/fibonacci.c.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+/tmp/bin 10
+/tmp/bin 20
