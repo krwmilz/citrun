2a398814b71e115ad49901e23f48f911f0b349e5
Kyle Milz <kyle@getaddrinfo.net>
2016-03-17 21:12:44 -0600

    viewer: remove glm includes

--- viewer/viewer.cxx
+++ viewer/viewer.cxx
@@ -6,11 +6,6 @@
 #include <GL/glew.h>
 #include <GL/freeglut.h>
 
-#define GLM_FORCE_RADIANS
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
-
 #include "af_unix.h"
 #include "text.h"
 
