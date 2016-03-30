ada9fa281b7163e65fce713d82124c99265fef41
Kyle Milz <kyle@getaddrinfo.net>
2016-03-29 22:58:40 -0600

    support linux

--- instrument/main.cc
+++ instrument/main.cc
@@ -81,7 +81,7 @@ instrument(int argc, char *argv[], std::vector<std::string> const &source_files)
 	// that will return a new MyFrontendAction object every time.  To
 	// further customize this, we could create our own factory class.
 	// int ret = Tool.run(new MFAF(inst_files));
-#if LLVM_VER > 35
+#if LLVM_VER > 34
 	int ret = Tool.run(&(*clang::tooling::newFrontendActionFactory<InstrumentAction>()));
 #else
 	int ret = Tool.run(clang::tooling::newFrontendActionFactory<InstrumentAction>());
