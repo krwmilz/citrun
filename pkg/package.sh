43e72eff38fcabd02659caa3e0082800eacc2417
kyle <kyle@getaddrinfo.net>
2016-04-02 15:43:26 -0600

    pkg: always check out our own ports tree

--- pkg/package.sh
+++ pkg/package.sh
@@ -1,29 +1,37 @@
 #!/bin/sh
 
+set -e
 ver=0
-
-# We need to share /usr/ports with other users
-umask 002
 
 echo archiving
 (cd .. && git archive --prefix=citrun-$ver/ -o pkg/citrun-$ver.tar.gz HEAD)
 
 if [ "`uname`" == "OpenBSD" ]; then
-	echo creating OpenBSD package
-	if [ ! -d /usr/ports ]; then
-		echo error: check out /usr/ports first
-		exit 1
+	echo OpenBSD detected
+
+	if [ ! -d ports ]; then
+		echo downloading OPENBSD_5_8 stable ports tree
+		curl -O http://ftp.openbsd.org/pub/OpenBSD/5.8/ports.tar.gz
+		echo ..extracting
+		tar xzf ports.tar.gz
+
+		echo overlaying CVS metadata and getting latest patches
+		cvs -qd anoncvs@anoncvs.usa.openbsd.org:/cvs get -rOPENBSD_5_8 -P ports
+	else
+		echo assuming ports/ tree already checked out
 	fi
-	cur_dir=`pwd`
-	# OpenBSD port building stuff needs to know where home is
-	export PORTSDIR_PATH="/usr/ports:$cur_dir"
-	# Tarball is located here
-	export DISTDIR=$cur_dir
+
+	echo copying port files into place
+	rm -rf ports/devel/citrun
+	cp -R openbsd ports/devel/citrun
+
+	echo creating package from citrun-$ver.tar.gz
+	export PORTSDIR_PATH=`pwd`/ports
+	export DISTDIR=`pwd`
 	# Disable tarball checksumming.
-	# Continuous integration does not like this kind of stuff.
 	export NO_CHECKSUM=1
-	make -C devel/citrun clean=all
-	make -C devel/citrun package
+	make -C ports/devel/citrun clean=all
+	make -C ports/devel/citrun package
 else
 	echo error: `uname` needs package magic
 fi
