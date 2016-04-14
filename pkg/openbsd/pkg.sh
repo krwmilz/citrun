19fc1f5e3d225f29411e7eff14fab6d7a6caacb0
Kyle Milz <kyle@getaddrinfo.net>
2016-04-13 18:25:57 -0600

    pkg: simplify openbsd packaging

--- pkg/openbsd/pkg.sh
+++ pkg/openbsd/pkg.sh
@@ -1,19 +1,18 @@
 #!/bin/sh -e
 
-ver=0
-pkgname="citrun"
-distname="${pkgname}-${ver}"
-distfile="$distname.tar.gz"
+portname="citrun"
 
 if [ "`uname`" != "OpenBSD" ]; then
 	echo "not making OpenBSD package on `uname`"
 	exit 1
 fi
 
-# Git won't archive the whole tree unless its called in the root..
-(cd ../../ && git archive --prefix=$distname/ -o /usr/ports/distfiles/$distfile HEAD)
-
 export PORTSDIR_PATH="`pwd`:/usr/ports"
 export NO_CHECKSUM=1
-make -C devel/$pkgname clean=all
-make -C devel/$pkgname package
+# Always re-fetch the latest sources
+rm /usr/ports/distfiles/citrun-0.tar.gz
+make -C devel/$portname clean=all
+make -C devel/$portname package
+
+doas pkg_delete citrun
+doas pkg_add -Dunsigned -r /usr/ports/packages/`uname -m`/all/${portname}-0.tgz
