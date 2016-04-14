8b188f42af9db0584e69c62c7ac114b929e006c9
Kyle Milz <kyle@getaddrinfo.net>
2016-04-13 21:31:46 -0600

    pkg: don't error when pkg_delete fails

--- pkg/openbsd/pkg.sh
+++ pkg/openbsd/pkg.sh
@@ -14,5 +14,5 @@ rm -f /usr/ports/distfiles/citrun-0.tar.gz
 make -C devel/$portname clean=all
 make -C devel/$portname package
 
-doas pkg_delete citrun
+doas pkg_delete citrun || true
 doas pkg_add -Dunsigned -r /usr/ports/packages/`uname -m`/all/${portname}-0.tgz
