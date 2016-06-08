1514881e1f327499c7e66f030b92b0325f6e535f
Kyle Milz <kyle@getaddrinfo.net>
2016-06-07 19:18:22 -0600

    pkg/openbsd: shuffle script around

--- pkg/openbsd/pkg.sh
+++ pkg/openbsd/pkg.sh
@@ -1,19 +1,23 @@
-#!/bin/sh -e
+#!/bin/sh
+
+set -e
+set -x
 
 portname="citrun"
 
-if [ "`uname`" != "OpenBSD" ]; then
-	echo "not making OpenBSD package on `uname`"
-	exit 1
-fi
+# Make sure package building doesn't rely on anything that's already installed
+doas pkg_delete $portname || true
 
+# Don't check checksums as this script is used for continuous integration
 export PORTSDIR_PATH="`pwd`:/usr/ports"
 export NO_CHECKSUM=1
+
 # Always re-fetch the latest sources
-rm -f /usr/ports/distfiles/citrun-0.tar.gz
+rm -f /usr/ports/distfiles/${portname}-0.tar.gz
+
+# The 'test' target will do a full build first
 make -C devel/$portname clean=all
 make -C devel/$portname test
 make -C devel/$portname package
 
-doas pkg_delete citrun || true
 doas pkg_add -Dunsigned -r /usr/ports/packages/`uname -m`/all/${portname}-0.tgz
