1aefc4029bc3c37719480d179a1f686bd1b0c764
Kyle Milz <kyle@getaddrinfo.net>
2016-06-11 20:41:23 -0600

    pkg/darwin: simplify portfile and use standard idioms

--- pkg/darwin/pkg.sh
+++ pkg/darwin/pkg.sh
@@ -7,7 +7,5 @@ portname="${1}"
 
 sudo port uninstall $portname
 
-rm /opt/local/var/macports/distfiles/$portname/$portname-0.tar.gz
-
-port -v -D devel/citrun clean
+sudo port -v -D devel/citrun clean --all
 sudo port -v -D devel/citrun install
