83c5d809b858aff10df7305bf8dcc984988d6929
Kyle Milz <kyle@0x30.net>
2016-06-22 22:10:59 -0600

    pkg/openbsd: be a bit more cruel when packaging

--- pkg/openbsd/pkg.sh
+++ pkg/openbsd/pkg.sh
@@ -4,9 +4,11 @@ set -e
 set -x
 
 portname="${1}"
+pkg_path=/usr/ports/packages/`uname -m`/all/${portname}-0.tgz
 
 # Make sure package building doesn't rely on anything that's already installed
 doas pkg_delete $portname || true
+rm -f $pkg_path
 
 # Don't check checksums as this script is used for continuous integration
 export PORTSDIR_PATH="`pwd`:/usr/ports"
@@ -20,4 +22,4 @@ make -C devel/$portname clean=all
 make -C devel/$portname test
 make -C devel/$portname package
 
-doas pkg_add -Dunsigned -r /usr/ports/packages/`uname -m`/all/${portname}-0.tgz
+doas pkg_add -Dunsigned -r $pkg_path
