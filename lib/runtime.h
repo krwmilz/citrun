fbb34b8d3390031322239650b28f9ee3f6788860
Kyle Milz <kyle@0x30.net>
2016-06-26 09:08:09 -0600

    lib: tweak struct citrun_node

--- lib/runtime.h
+++ lib/runtime.h
@@ -1,10 +1,10 @@
 #include <stdint.h>
 struct citrun_node {
-	uint64_t *lines_ptr;
-	uint32_t size;
-	uint32_t inst_sites;
-	const char *file_name;
-	struct citrun_node *next;
+	uint64_t		*lines_ptr;
+	uint32_t		 size;
+	uint32_t		 inst_sites;
+	const char		*file_name;
+	struct citrun_node	*next;
 };
 void citrun_node_add(struct citrun_node *);
 void citrun_start();
