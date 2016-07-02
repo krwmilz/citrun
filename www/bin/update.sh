09685ad88b6ca403dca17dcec2a894b034537b4e
Kyle Milz <kyle@0x30.net>
2016-07-02 17:53:58 -0600

    www: change text to "e2e report"

--- www/bin/update.sh
+++ www/bin/update.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sed -e "s%stale ${1}\">stale%${1}\"><a href=\"/pkg/${2}\">test report</a>%" -i index.html
+sed -e "s%stale ${1}\">stale%${1}\">e2e <a href=\"/pkg/${2}\">report</a>%" -i index.html
