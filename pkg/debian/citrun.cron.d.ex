eb1a28924a327c71cba9a7a5e0e01a468350c763
Kyle Milz <kyle@cyan.krwm.net>
2016-07-03 15:11:08 -0600

    debian: add packaging stuff

--- pkg/debian/citrun.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the citrun package
-#
-0 4	* * *	root	[ -x /usr/bin/citrun_maintenance ] && /usr/bin/citrun_maintenance
--- pkg/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(citrun):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="citrun" command="/usr/bin/citrun"
