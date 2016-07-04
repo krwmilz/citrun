657119c465527457ab03601df6a30f61d3707f85
Kyle Milz <kyle@0x30.net>
2016-07-03 22:15:19 -0600

    pkg: move everything into a single script

--- pkg.sh
+++ pkg.sh
@@ -1,17 +1,52 @@
 #!/bin/sh
 
-# This script exists so that continuous integration has a single point of entry
-# for building packages on all platforms.
+set -e
+set -x
 
-uname_lc=`uname | tr '[:upper:]' '[:lower:]'`
-if [ ! -d $uname_lc ]; then
-	echo Error: Need packaging directory for "$uname_lc"
-	exit 1;
-fi
-
+# Single point of entry for building packages on all platforms.
+#
 if [ "${1}" != "citrun" -a "${1}" != "ccitrunrun" ]; then
-	echo Error: package name must be \'citrun\' or \'ccitrunrun\'
 	exit 2;
 fi
 
-(cd $uname_lc && sh pkg.sh ${1})
+portname="${1}"
+uname=`uname`
+
+if [ "$uname" = "OpenBSD" ]; then
+	pkg_path=/usr/ports/packages/`uname -m`/all/${portname}-0.tgz
+
+	# Make sure package building doesn't rely on anything that's already installed
+	doas pkg_delete $portname || true
+	rm -f $pkg_path
+
+	# Don't check checksums as this script is used for continuous integration
+	export PORTSDIR_PATH="`pwd`/openbsd:/usr/ports"
+	export NO_CHECKSUM=1
+
+	# Always re-fetch the latest sources
+	rm -f /usr/ports/distfiles/${portname}-0.tar.gz
+
+	# The 'test' target will do a full build first
+	make -C openbsd/devel/$portname clean=all
+	make -C openbsd/devel/$portname test
+	make -C openbsd/devel/$portname package
+
+	doas pkg_add -Dunsigned -r $pkg_path
+
+elif [ "$uname" = "Darwin" ]; then
+	sudo port uninstall $portname
+
+	sudo port -v -D darwin/devel/citrun clean
+	sudo port -v -D darwin/devel/citrun test
+	sudo port -v -D darwin/devel/citrun install
+
+elif [ "$uname" = "Linux" ]; then
+	echo ""
+
+else
+	echo Error: Can\'t package for unknown system \"$uname\"
+	exit 1;
+fi
+
+# Reset end to end report when new packages are installed.
+rm -f e2e_report.txt
