92fdc2de421fc465230629ce114702822bcba0fd
Kyle Milz <kyle@0x30.net>
2016-07-13 23:18:10 -0600

    src: remove struct point

--- src/draw.h
+++ src/draw.h
@@ -1,15 +1,6 @@
 #ifndef DRAW_H
 #define DRAW_H
 
-#include <GL/glew.h>
-
-struct point {
-	GLfloat x;
-	GLfloat y;
-	GLfloat s;
-	GLfloat t;
-};
-
 class drawable {
 public:
 	virtual void draw() = 0;
