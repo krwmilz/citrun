e377703b4e9f1a59f05707a4c2015400f3e2d7d0
Kyle Milz <kyle@0x30.net>
2016-08-19 23:42:17 -0600

    t: fix this bad bad test

--- t/inst_basic_link.sh
+++ t/inst_basic_link.sh
@@ -1,7 +1,7 @@
 #
 # Check that the most basic of compile command lines works.
 #
-echo 1..5
+echo 1..4
 . test/utils.sh
 
 cat <<EOF > main.c
@@ -9,32 +9,22 @@ int main(void) { return 0; }
 EOF
 echo "ok 2 - source files wrote"
 
-# Check that a command as simple as this works.
-#
 $TEST_TOOLS/citrun-wrap cc main.c
 echo "ok 3 - source compiled"
 
-$TEST_TOOLS/citrun-check > citrun-check.txt
-echo "ok 4 - processed citrun.log"
-
-cat <<EOF > citrun-check.txt.good
-Checking ..done
-
+cat <<EOF > check.good
 Summary:
-         1 Log files found
-         1 Source files input
-         1 Calls to the instrumentation tool
-         1 Forked compilers
-         1 Instrument successes
+         1 Calls to the rewrite tool
+         1 Source files used as input
          1 Application link commands
+         1 Rewrite successes
+         1 Rewritten source compile successes
 
 Totals:
          2 Lines of source code
-         1 Functions called 'main'
          1 Function definitions
          1 Return statement values
          3 Total statements
 EOF
-
-diff -u citrun-check.txt.good citrun-check.txt
-echo "ok 5 - citrun.log diff"
+$TEST_TOOLS/citrun-check > check.out
+check_diff 4
