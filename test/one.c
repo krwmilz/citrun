4ea3d2055c3103e5f95ccde2ff71ad9e49590f23
Kyle Milz <kyle@0x30.net>
2016-08-29 18:10:32 -0600

    test: compile test program with build

---
+++ test/one.c
@@ -0,0 +1,25 @@
+#include <err.h>
+#include <stdlib.h>
+
+long long fib(long long);
+void print_output(long long);
+
+void
+usr1_sig(int signal)
+{
+	exit(0);
+}
+
+int
+main(int argc, char *argv[])
+{
+	long long n;
+
+	if (argc != 2)
+		errx(1, "argc != 2");
+
+	n = atoi(argv[1]);
+
+	print_output(fib(n));
+	return 0;
+}
---
+++ test/three.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void
+print_output(long long n)
+{
+	fprintf(stderr, "%lli", n);
+	return;
+}
