61f28ad5221ac7d627c68fbdd8daa35d33960bd3
Kyle Milz <kyle@0x30.net>
2016-12-11 13:21:06 -0700

    src: clean up class definiton

--- src/process_dir.h
+++ src/process_dir.h
@@ -5,13 +5,13 @@
 #include <unordered_set>
 #include <vector>
 
-class ProcessDir {
-public:
-	ProcessDir();
-	std::vector<std::string>	 scan();
-
-private:
+class ProcessDir
+{
 	const char			*m_procdir;
 	DIR				*m_dirp;
 	std::unordered_set<std::string>	 m_known_files;
+
+public:
+	ProcessDir();
+	std::vector<std::string>	 scan();
 };
