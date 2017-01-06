cb9dc1bb7cdb27091b20b2f93cf580e9fd811caf
Kyle Milz <kyle@windows.krwm.net>
2017-01-05 21:24:18 -0800

    configure: read llvm-config stdout into variables

--- configure.bat
+++ configure.bat
@@ -2,9 +2,6 @@
 REM
 REM Checks that a bunch of crap is installed and available on Windows.
 REM
-ECHO.
-ECHO !! Windows configuration script running.
-ECHO.
 
 REM
 REM Need to 'setlocal' otherwise Path modifications get saved to the parent shell.
@@ -52,8 +49,6 @@ IF %ERRORLEVEL% NEQ 0 (
 )
 
 
-SET CLANG_LIBS=clangAST.lib clangAnalysis.lib clangBasic.lib clangDriver.lib clangEdit.lib clangFrontend.lib clangFrontendTool.lib clangLex.lib clangParse.lib clangRewrite.lib clangRewriteFrontend.lib clangSema.lib clangSerialization.lib clangTooling.lib
-SET LLVM_LIBS=bitreader mcparser transformutils option
 
 DEL Jamrules 2>nul
 
@@ -69,28 +64,31 @@ REM GL_CFLAGS = `pkg-config --cflags glfw3 glew freetype2` ;
 REM GL_LIBS = ${GL_EXTRALIB-} `pkg-config --libs glfw3 glew freetype2` ;
 REM GLTEST_LIBS  = `pkg-config --libs osmesa` ;
 
->> Jamrules ECHO INST_CFLAGS =
->> Jamrules ECHO -IC:\\Clang\\include
->> Jamrules llvm-config --cxxflags
->> Jamrules ECHO ;
+>  llvm-config.out llvm-config --cxxflags
+SET /p inst_cflags=<llvm-config.out
+
+>> Jamrules ECHO INST_CFLAGS = -IC:\\Clang\\include %inst_cflags:\=\\% ;
 >> Jamrules ECHO.
 
->> Jamrules ECHO INST_LDFLAGS =
->> Jamrules ECHO -LIBPATH:C:\\Clang\\lib
->> Jamrules llvm-config --ldflags
->> Jamrules ECHO ;
+>  llvm-config.out llvm-config --ldflags
+SET /p inst_ldflags=<llvm-config.out
+
+>> Jamrules ECHO INST_LDFLAGS = -LIBPATH:C:\\Clang\\lib %inst_ldflags:\=\\% ;
 >> Jamrules ECHO.
 
->> Jamrules ECHO INST_LIBS =
->> Jamrules ECHO %CLANG_LIBS%
->> Jamrules llvm-config --libnames %LLVM_LIBS%
->> Jamrules llvm-config --system-libs
+>  llvm-config.out llvm-config --libnames bitreader mcparser transformutils option
+>> llvm-config.out llvm-config --system-libs
+SET /p inst_libs_llvm=<llvm-config.out
+
+SET inst_libs_clang=clangAST.lib clangAnalysis.lib clangBasic.lib clangDriver.lib clangEdit.lib clangFrontend.lib clangFrontendTool.lib clangLex.lib clangParse.lib clangRewrite.lib clangRewriteFrontend.lib clangSema.lib clangSerialization.lib clangTooling.lib
+
+>> Jamrules ECHO INST_LIBS = %inst_libs_clang% %inst_libs_llvm%
 >> Jamrules ECHO shlwapi.lib version.lib ;
 >> Jamrules ECHO.
+>> Jamrules ECHO include Jamrules.tail ;
 
-REM Append Jamrules.tail to generated Jamrules.
-COPY /b Jamrules + Jamrules.tail Jamrules >nul
-
+DEL llvm-config.out
 ENDLOCAL
-ECHO !! Jamrules written, configuration is complete.
+
+ECHO !! C It Run Windows configuration script finished.
 PAUSE
