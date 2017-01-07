e5450783ec27001f11b5d52ba62d588e58fb7d4d
Kyle Milz <kyle@windows.krwm.net>
2017-01-07 01:23:13 -0800

    wrap: pass CITRUN_PATH onto citrun_inst

--- wrap.bat
+++ wrap.bat
@@ -1,7 +1,13 @@
 @ECHO off
 
 SETLOCAL
-SET Path=_CITRUN_COMPILERS_;%Path%
+
+:: CITRUN_PATH is used in citrun_inst to find the runtime library and compilers.
+:: Hook the PATH with our directory that conveniently has a cl.exe in it.
+SET CITRUN_PATH=%~dp0
+SET Path=%CITRUN_PATH%compilers;%Path%
+
 CALL %*
 EXIT /B %ERRORLEVEL%
+
 ENDLOCAL
