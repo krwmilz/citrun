40af015cca1882a849d3b2e7b8286025813d2d16
Kyle Milz <kyle@windows.krwm.net>
2017-01-08 16:02:21 -0800

    configure: add more style

--- configure.bat
+++ configure.bat
@@ -1,67 +1,57 @@
 @ECHO off
-REM
-REM Checks that a bunch of crap is installed and available on Windows.
-REM
+ECHO ░█▀▀░░░▀█▀░▀█▀░░░█▀▄░█░█░█▀█
+ECHO ░█░░░░░░█░░░█░░░░█▀▄░█░█░█░█
+ECHO ░▀▀▀░░░▀▀▀░░▀░░░░▀░▀░▀▀▀░▀░▀
+ECHO Configuring...
+ECHO.
 
-REM
-REM Need to 'setlocal' otherwise Path modifications get saved to the parent shell.
-REM Extends to end of file so we have these modifications the entire time.
-REM
+::
+:: Need to 'setlocal' otherwise Path modifications get saved to the parent shell.
+:: Extends to end of file so we have these modifications the entire time.
+::
 SETLOCAL
 SET Path=C:\LLVM\bin;%Path%
 
-REM
-REM Check binaries needed for building are available.
-REM
-
-WHERE cl >nul
+::
+:: Check binaries needed for building are available.
+::
+WHERE cl
 IF %ERRORLEVEL% NEQ 0 (
 	ECHO cl.exe not found on the Path!
+	ECHO Please make sure you run this script from a Developer Command Prompt.
 	ECHO.
-	ECHO Please make sure Visual Studio is installed and that you're running this
-	ECHO at a developer command prompt with the correct Path set.
+	PAUSE
 	EXIT /B 1
 )
 
-WHERE jam >nul
+WHERE jam
 IF %ERRORLEVEL% NEQ 0 (
 	ECHO jam.exe not found on the Path!
+	PAUSE
 	EXIT /B 1
 )
 
-WHERE llvm-config >nul 2>nul
+WHERE llvm-config 2>nul
 IF %ERRORLEVEL% NEQ 0 (
 	ECHO llvm-config.exe not found on the Path!
 	ECHO.
 	ECHO I had to compile LLVM sources to get this  executable.
 	ECHO.
 	ECHO Download LLVM sources from http://llvm.org/.
+	PAUSE
 	EXIT /B 1
 )
-
-WHERE perl >nul
-IF %ERRORLEVEL% NEQ 0 (
-	ECHO Perl not found.
-	ECHO
-	ECHO Consider installing Strawberry Perl from
-	ECHO http://strawberryperl.com
-	EXIT /B 1
-)
-
-
-
-DEL Jamrules 2>nul
 
 :: Silence warning about exceptions being disabled from xlocale.
->> Jamrules ECHO C++FLAGS = /D_HAS_EXCEPTIONS=0 ;
+>  Jamrules ECHO C++FLAGS = /D_HAS_EXCEPTIONS=0 ;
 >> Jamrules ECHO.
 
 >> Jamrules ECHO FONT_PATH = "C:\Windows\Fonts\consola.ttf" ;
 >> Jamrules ECHO.
 
-REM GL_CFLAGS = `pkg-config --cflags glfw3 glew freetype2` ;
-REM GL_LIBS = ${GL_EXTRALIB-} `pkg-config --libs glfw3 glew freetype2` ;
-REM GLTEST_LIBS  = `pkg-config --libs osmesa` ;
+:: GL_CFLAGS = `pkg-config --cflags glfw3 glew freetype2` ;
+:: GL_LIBS = ${GL_EXTRALIB-} `pkg-config --libs glfw3 glew freetype2` ;
+:: GLTEST_LIBS  = `pkg-config --libs osmesa` ;
 
 >  llvm-config.out llvm-config --cxxflags
 SET /p inst_cflags=<llvm-config.out
@@ -83,9 +73,6 @@ SET inst_libs_clang=clangAST.lib clangAnalysis.lib clangBasic.lib clangDriver.li
 
 >> Jamrules ECHO INST_LIBS = %inst_libs_clang% %inst_libs_llvm%
 >> Jamrules ECHO shlwapi.lib version.lib ;
->> Jamrules ECHO.
->> Jamrules ECHO include Jamrules.tail ;
 
 DEL llvm-config.out
 ENDLOCAL
-PAUSE
