de01cd8efe9fa959c3f05a69251fb3f20ab33f42
Kyle Milz <kyle@0x30.net>
2017-01-10 23:40:41 -0700

    www: add sync script

--- distrib/www.sh
+++ distrib/www.sh
@@ -3,6 +3,8 @@
 mkdir -p www/man
 
 mandoc_cmd="mandoc -Thtml -Ostyle=/citrun.css -Oman=%N.%S.html"
-for man in man/*.1; do
+for man in *.1; do
         $mandoc_cmd $man > www/man/`basename $man`.html
 done
+
+scp -r www/* 0x30.net:/var/www/htdocs/citrun.com/
