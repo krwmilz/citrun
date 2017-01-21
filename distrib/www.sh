0d85081f81ae9ca996b33eee9b62023e6a0c30fe
kyle <kyle@0x30.net>
2017-01-21 14:25:50 -0700

    distrib: remove command indirection

--- distrib/www.sh
+++ distrib/www.sh
@@ -1,10 +1,9 @@
 #!/bin/sh -e
 
 mkdir -p www/man
-
-mandoc_cmd="mandoc -Thtml -Ostyle=/citrun.css -Oman=%N.%S.html"
 for man in *.1; do
-        $mandoc_cmd $man > www/man/`basename $man`.html
+        mandoc -Thtml -I os="TempleOS 5.01" -Ostyle=/citrun.css -Oman=%N.%S.html \
+		$man > www/man/`basename $man`.html
 done
 
 scp -p -r www/* 0x30.net:/var/www/htdocs/citrun.com/
